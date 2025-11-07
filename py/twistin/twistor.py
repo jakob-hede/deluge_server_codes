b35bd780814a7e3a66f01ae587c0a5565afcff16
sermin asusi7 <sermin@asusi7.machine>
2025-11-07 13:40:39 +0100

    twistin

--- py/twistin/twistor.py
+++ py/twistin/twistor.py
@@ -1,7 +1,31 @@
+from __future__ import annotations
 from typing import Generator, Any
 
 from twisted.internet import reactor, defer, task
 from executin.logge import Loggor
+
+class TwistinException(Exception):
+    """Exception raised for errors in the Twistin module."""
+
+    def x__str__(self):
+        super_str = super().__str__()
+        txt = (f"\n- {self.__class__.__name__}:"
+               f"\n\t - args: {self.args}"
+               f"\n\t - super_str: {super_str}"
+               )
+        return txt
+
+    @classmethod
+    def factory_from_exception(cls, e: Exception, *args) -> TwistinException:
+        enfo = f'{e.__class__.__name__}: {e.args}'
+        args_str = ', '.join([str(a) for a in args])
+        exc = cls(f"Wrapped exception: {enfo} |  {args_str}")
+        return exc
+
+
+class TwistinTestException(TwistinException):
+    """Exception raised for transaction errors in the Twistin module."""
+    pass
 
 
 class Twistor:
@@ -96,13 +120,20 @@ class Twistor2(Twistor):
             yield task.deferLater(reactor, 1, lambda: None)
             self.loggor.debug('Async dummy process completed')
             reply: dict = {'status': 'success', 'duration': 1}
+
+            exc = TwistinTestException('Simulated exception in reactize')
+            # exc = Exception('Simulated exception in reactize')
+            raise exc
+
             defer.returnValue(reply)
 
         try:
             result = yield reactize()
             self.loggor.info(f'reactize completed with result: {result}')
-        except Exception as e:
+        except TwistinException as e:
             self.loggor.error(f'reactize failed: {e}')
+        except Exception as e:
+            self.loggor.error(f'UNKNOWN reactize failed: {e}')
         finally:
             self.loggor.debug('finally block reached')
             reactor.stop()  # noqa
